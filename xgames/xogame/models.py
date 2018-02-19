b64c0c7e67bebdcb5e31e9ecc0a231c297184799
aalfahad <boomar14@hotmail.com>
2018-02-19 20:29:55 +0300

    first break

--- xgames/xogame/models.py
+++ xgames/xogame/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+	name = models.CharField(max_length='225')
+	release_date = models.DateField()
+	multiplayer = models.BooleanField()
+	image = models.ImageField()
+
+	PC = 'PC'
+	phones = 'ph'
+	Xbox = 'Xb'
+	Web = 'Wb'
+	platform_choices = (
+		(PC, 'PC'),
+		(phones, 'smartphones'),
+		(Xbox, 'Xbox one'),
+		(Web, 'Web Browser'),
+	)
+	platforms = models.CharField(max_length=2,choices=platform_choices,default='PC')
